bbd5ab9492d07d7478ce551ae570d2c9dd666fc3
a.izyckim <zamf124@gmail.com>
2018-06-18 19:52:31 +0200

    preparing for main project

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./Layout.css";
+const layout = props => (
+  <React.Fragment>
+    <div> Toolbar, SideDawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </React.Fragment>
+);
+
+export default layout;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,56 +1,15 @@
 import React, { Component } from "react";
 import classes from "./App.css";
-import Persons from "./../components/Persons/Persons";
-import Cockpit from "./../components/Cockpit/Cockpit";
+import Layout from "../components/Layout/Layout";
+import BurgerBuilder from "../containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
-  state = {
-    persons: [
-      { id: 1, name: "Arek", age: "30" },
-      { id: 2, name: "Marta", age: "26" },
-      { id: 3, name: "Agnieszka", age: "29" }
-    ],
-    showPersons: false
-  };
-  togglePersonsHandler = event => {
-    this.setState({ showPersons: !this.state.showPersons });
-  };
-
-  nameChanger = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => p.id === id);
-    const person = { ...this.state.persons[personIndex] };
-    person.name = event.target.value;
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-    this.setState({ persons });
-  };
-
-  deletePerson = personIndex => {
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({ persons });
-  };
-
   render() {
-    let persons = null;
-
-    if (this.state.showPersons) {
-      persons = (
-        <Persons
-          persons={this.state.persons}
-          cicked={this.deletePerson}
-          changed={this.nameChanger}
-        />
-      );
-    }
     return (
       <div className={classes.App}>
-        <Cockpit
-          clicked={this.togglePersonsHandler}
-          showPersons={this.state.showPersons}
-          persons={this.state.persons}
-        />
-        {persons}
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
